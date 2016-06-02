40ebfd286dc030c61af4be4b5a236989e620fa8e
Tristan Van Berkom <tristan.vanberkom@codethink.co.uk>
2016-06-02 16:22:01 +0900

    irc-notify.py: Adding -t option
    Allows specifying regular/success/fail messages (just changes color of message)

--- extra/irc-notify.py
+++ extra/irc-notify.py
@@ -26,6 +26,11 @@ from twisted.internet import defer, endpoints, protocol, reactor, task
 from twisted.python import log
 from twisted.words.protocols import irc
 
+GREEN = 3
+RED   = 4
+def irc_color(code, S):
+    return "\x03%d%s\x03" % (code, S)
+
 class FlatpakIRCProtocol(irc.IRCClient):
 
     def __init__(self):
@@ -43,7 +48,13 @@ class FlatpakIRCProtocol(irc.IRCClient):
         for channel in self.factory.channels:
             if args.join:
                 self.join(channel)
-            self.msg(channel, args.message)
+
+            if args.type == 'success':
+                self.msg(channel, irc_color (GREEN, args.message))
+            elif args.type == 'fail':
+                self.msg(channel, irc_color (RED, args.message)) 
+            else:
+                self.msg(channel, args.message)
 
         self.quit()
 
@@ -66,11 +77,18 @@ def main(reactor, description):
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument ('-s',       dest='server',  required=True,        help="The IRC server to connect to")
-    parser.add_argument ('-p',       dest='port',    default='6667',       help="The port to connect to (default: 6667)")
-    parser.add_argument ('-c',       dest='channel', required=True,        help="The channel to announce in")
-    parser.add_argument ('-n',       dest='nick',    required=True,        help="The nickname to use")
-    parser.add_argument ('--nojoin', dest='join',    action='store_false', help="Send the message to channel without joining")
+    parser.add_argument ('-s', dest='server', required=True,
+                         help="The IRC server to connect to")
+    parser.add_argument ('-p', dest='port', default='6667',
+                         help="The port to connect to (default: 6667)")
+    parser.add_argument ('-c', dest='channel', required=True,
+                         help="The channel to announce in")
+    parser.add_argument ('-n', dest='nick', required=True,
+                         help="The nickname to use")
+    parser.add_argument ('-t', dest='type', default='regular', choices=['success', 'fail', 'regular'],
+                         help="Type of message to send")
+    parser.add_argument ('--nojoin', dest='join', action='store_false',
+                         help="Send the message to channel without joining")
     parser.add_argument ('message')
     args = parser.parse_args()
 
