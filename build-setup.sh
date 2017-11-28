297a5f9be551277d9ffca14d66f20809260652b9
Tristan Van Berkom <tristan.vanberkom@codethink.co.uk>
2017-11-28 17:45:39 +0000

    build-setup.sh: Pointing the flatpak build to a commit just after 0.10.1
    The new commit is required to build on the arm builders, as it lets
    you build with appstream-glib requirement at older versions.

--- build-setup.sh
+++ build-setup.sh
@@ -162,7 +162,7 @@ function refreshTools() {
     # Refresh the build tooling itself with the autotools stuff
     #
     buildSourceAdd "ostree"     "https://github.com/ostreedev/ostree"    "6f1bf70a769204f0627a9d499ec9cf223947a325" buildInstallAutotools
-    buildSourceAdd "flatpak"    "https://github.com/flatpak/flatpak.git" "0.10.1" buildInstallAutotools
+    buildSourceAdd "flatpak"    "https://github.com/flatpak/flatpak.git" "50fbafa20258e9afc6207984edea6cd4307d1bee" buildInstallAutotools
     buildSourceAdd "flatpak-builder"    "https://github.com/flatpak/flatpak-builder.git" "0.10.5" buildInstallAutotools
 
     buildSourceRun
